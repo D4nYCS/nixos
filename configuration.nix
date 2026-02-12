64cc5a8cf9a7fd9c26d9b7defa50754bae706214
D4nYCS <danielmaier2001@yahoo.de>
2026-02-12 22:25:07 +0100

    1

--- configuration.nix
+++ configuration.nix
@@ -99,8 +99,6 @@
     spiceUSBRedirection.enable = true;
   };
 
-  services.spice-vdagentd.enable = true;
-
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
