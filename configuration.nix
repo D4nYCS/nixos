bd72dfca573e2baccb364d35435f8bc55d96de68
D4nYCS <danielmaier2001@yahoo.de>
2026-02-13 13:52:39 +0100

    edited configuration and added gitconfig_sample

--- configuration.nix
+++ configuration.nix
@@ -8,7 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ./macos_broadcom_package.nix
+      ./macbook-configuration.nix
     ];
 
   # Bootloader.
